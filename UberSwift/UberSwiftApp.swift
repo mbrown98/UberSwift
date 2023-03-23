3572fd3c13575a9b21a287177ac7645f40e949d4
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2023-03-23 17:40:50 -0400

    Initial Commit

---
+++ UberSwift/UberSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftApp.swift
+//  UberSwift
+//
+//  Created by Matt Brown on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
